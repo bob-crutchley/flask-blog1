84977933e94472d4a83bdf76752f8eae49ac0dff
Chester Gardner <chester.gardner@qa.com>
2019-08-15 23:31:57 +0100

    first commit

---
+++ application/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = '7218a9143c27c16610765205a1b21cb7'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from application import routes
